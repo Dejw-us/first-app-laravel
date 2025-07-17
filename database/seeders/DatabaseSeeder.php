9791b5beca9280eb79d6c32d4337f23d444b9a82
Dawid Ratajczak <dawid.ratajczak06+github@gmail.com>
2025-07-17 17:12:19 +0200

    Adding breeze

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Models\Note;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    User::factory()->create([
+      'id' => 1,
+      'password' => bcrypt('pass123.'),
+      'name' => 'Test User',
+      'email' => 'test@example.com',
+    ]);
+
+    Note::factory(100)->create();
+  }
 }
